1d033cb3deeb9325aaa5ec14b876370f6cd82081
yeonwoo780 <rlckrl0315db@gmail.com>
2025-12-09 08:18:09 +0000

    Qwen-Image-MathAI-Korean-Drawing-v02 safetensor py update

---
+++ model_safetensor_down/Qwen-Image-MathAI-Korean-Drawing-v02.py
@@ -0,0 +1,42 @@
+from huggingface_hub import hf_hub_download
+import shutil
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+repo_id = "MathAI-Company/Qwen-Image-MathAI-Korean-Drawing-v02"
+filename = "Qwen-Image-MathAI-Korean-Drawing-v02.safetensors"
+local_dir="/home/mathai/code/ComfyUI/models/loras/"
+
+token = os.getenv("HUGGINGFACE_ACCESS_TOKEN_READ")
+
+local_path = hf_hub_download(
+    repo_id=repo_id, 
+    filename=filename,
+    local_dir=local_dir,
+    local_dir_use_symlinks=False,
+    token=token
+)
+
+basename = os.path.basename(filename)
+
+print(f"SafeTensors file downloaded to: {local_path}")
+
+# 3) 최종적으로 원하는 위치 = local_dir/<파일명>
+target_path = os.path.join(local_dir, basename)
+
+# 4) 만약 구조가 포함된 경로로 저장되었으면 파일만 복사
+if local_path != target_path:
+    shutil.copy2(local_path, target_path)
+    os.remove(local_path)
+
+    dir_path = os.path.dirname(local_path)
+    while dir_path.startswith(local_dir) and dir_path != local_dir:
+        try:
+            os.rmdir(dir_path)  # 빈 디렉토리만 삭제됨
+        except OSError:
+            break  # 비어있지 않으면 즉시 종료
+        dir_path = os.path.dirname(dir_path)
+
+print("최종 저장 경로:", target_path)
